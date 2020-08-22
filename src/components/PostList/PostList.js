a071377ae3a85f16c8761443936985645af91b4e
Minh Nguyen <nguyen.715@osu.edu>
2020-08-22 18:07:08 -0400

    removed unused code

--- src/components/PostList/PostList.js
+++ src/components/PostList/PostList.js
@@ -26,7 +26,7 @@ export default function PostList() {
     <>
       { context.email && (
         <>
-          <div id="shareable-link">Shareable URL for your list: {shareableLink}</div>
+          <div id="shareable-link">Shareable URL for your list: <a href={shareableLink} target="_blank" rel="noopener noreferrer">{shareableLink}</a></div>
 
           <Link to="/create-post">
             <button className="create-post-button">
